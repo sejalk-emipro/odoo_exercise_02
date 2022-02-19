fdddf55880f5e3b7e9d4b1dc7af045252c8ce9e4
Sejalk <sejalk@emiprotechnologies.com>
2022-02-19 12:58:38 +0530

    [ADD] Commit exercise 2 requirement 1

---
+++ res_localization_ept/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding :utf-8 -*-
+{
+    'name':'res_localization_ept',
+    'category':'Utility',
+    'version':'1.1',
+    'author':'Emipro Technology Pvt. Ltd.',
+    'description':"""
+    This module for the demonstration of the State data
+    """,
+    'website':'https://www.emiprotechnologies.com',
+    'depends':['sales_team'],
+    'data':[],
+    'demo':[],
+    'sequence':12,
+    'auto_install':False,
+    'application':False,
+    'installable':True
+}
